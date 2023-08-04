717d6bb02f56b3e0e851625eedd18cf33f492f7d
debiabril <debora.badosa@gmail.com>
2023-08-04 11:47:05 -0300

    carousel más dinámico sin useEfect aún

--- src/pages/Index/Home.jsx
+++ src/pages/Index/Home.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import HeroCallToAction from '../../components/HeroCallToAction'
 import Carousel from '../../components/Carousel'
+import '../../styles/Home.css'
+
 
 export default function Home() {
   return (
