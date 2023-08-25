d496d9b85e9cbeb1ccf815fb69c47cb49b01e7ae
debiabril <debora.badosa@gmail.com>
2023-08-25 11:40:05 -0300

    agregando svgs como componentes

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import Location from './svgs/Location.jsx'
+
 export function Cards({city, country, url}) {
  
     return(
@@ -13,7 +15,7 @@ export function Cards({city, country, url}) {
                     {city}
                     </p>
                     <div className='flex items-center'>
-                    <svg className="h-4 stroke-cyan-400" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M12 21C15.5 17.4 19 14.1764 19 10.2C19 6.22355 15.866 3 12 3C8.13401 3 5 6.22355 5 10.2C5 14.1764 8.5 17.4 12 21Z" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path><path d="M12 12C13.1046 12 14 11.1046 14 10C14 8.89543 13.1046 8 12 8C10.8954 8 10 8.89543 10 10C10 11.1046 10.8954 12 12 12Z" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path></svg>
+                    <Location/>
                     <p className="text-lg pl-1 drop-shadow-darkShadow font-bold tracking-widest  text-cyan-400 md:text-2xl">
                     {country}
                     </p>
--- src/components/DetailsCard.jsx
+++ src/components/DetailsCard.jsx
@@ -16,15 +16,18 @@ export default function DetailsCard() {
   return (
     <>
       <div className='bg-cover bg-center min-h-[80vh] text-white' style={{ backgroundImage: `url('${cities?.url}')` }}>
-      <div className='backdrop-blur-sm py-12 min-h-[50vh] flex flex-col justify-center rounded-3xl'> 
-      <h2 className='text-center text-4xl py-1 '>{cities?.city}</h2>
-      <h3 className='text-center text-3xl py-1' >{cities?.country}</h3>
-      <h4 className='text-center text-xl py-1'>Language:  {cities?.language}</h4>
-      <h5 className='text-center text-lg py-1'>Currency:  {cities?.currency}</h5>
-      <p className='px-4 md:px-52 text-center'>{cities?.description }</p>
-      <LinkRouter to='/cities' className='navbar-link text-black drop-shadow-darkShadow hover:text-purple-800 hover:drop-shadow-none flex self-end mr-14 border border-solid rounded-lg border-indigo-400 bg-cyan-300 px-3 py-2'> Go back </LinkRouter>
-      </div>
-      <UnderContruction/>
+        <div className='backdrop-blur-sm py-12 min-h-[50vh] flex flex-col justify-center rounded-3xl'> 
+        <h2 className='text-center text-4xl py-1 '>{cities?.city}</h2>
+        <h3 className='text-center text-3xl py-1' >{cities?.country}</h3>
+        <h4 className='text-center text-xl py-1'>Language:  {cities?.language}</h4>
+        <h5 className='text-center text-lg py-1'>Currency:  {cities?.currency}</h5>
+        <p className='px-4 md:px-52 text-center'>{cities?.description }</p>
+          <div className='flex justify-end my-2 py-2'>
+          <LinkRouter to='/cities' className='navbar-link text-black drop-shadow-darkShadow hover:text-purple-800 hover:drop-shadow-none flex self-end mr-14 border border-solid rounded-lg border-indigo-400 bg-cyan-300 px-3 py-2'> Go back </LinkRouter>
+          <LinkRouter to='/' className='navbar-link text-black drop-shadow-darkShadow hover:text-purple-800 hover:drop-shadow-none flex self-end mr-14 border border-solid rounded-lg border-indigo-400 bg-cyan-300 px-3 py-2'> Go home </LinkRouter>
+          </div>
+        </div>
+        <UnderContruction/>
       </div>
     </>
   )
