5ead2ef4b3d7d164331ec59eebe379989512f21a
debiabril <debora.badosa@gmail.com>
2023-09-07 12:36:18 -0300

    problema de asincronismo al cargar los usuarios resualto

--- src/components/ItineraryCity.jsx
+++ src/components/ItineraryCity.jsx
@@ -15,25 +15,26 @@ export default function ItineraryCity({ _id} ) {
   const dispatch = useDispatch()
     useEffect(()=>{
         dispatch(get_itinerary_by_id(_id)) 
+        
     },[])
     return (
     <>
         <div className='flex justify-center'>
         <div className='min-h-[30vh] w-[60vw]'>
             <div className='min-h-[10vh] flex justify-center bg-teal-700 my-2 rounded-lg p-2'>
-                <h2 className='text-3xl mt-3'>{itinerary.name} </h2>
+                <h2 className='text-3xl mt-3'>{itinerary?.name} </h2>
             </div>
             <div className='min-h-[15vh] flex justify-center bg-teal-700 my-2 rounded-lg p-2'> 
                 <div className='w-[30vw] flex self-center flex-col ml-3' > <div className='text-lg'> Price :  
-                    <ItineraryPrice price={itinerary.price} />
-                </div><div><h2 className='text-lg'>Duration: {itinerary.duration} hours</h2> </div></div>
+                    <ItineraryPrice price={itinerary?.price} />
+                </div><div><h2 className='text-lg'>Duration: {itinerary?.duration} hours</h2> </div></div>
                 <div className='w-[30vw] flex justify-center' > 
-                <UserItinerary id={itinerary.user}/>
+                <UserItinerary id={itinerary?.user} name= {itinerary?.user?.name} image = {itinerary?.user?.image}/>
                 </div>
                 <div className='w-[20vw] flex justify-center flex-col mr-3' > 
-                    <div><h2 className='text-center text-lg'>{itinerary.hashtags}</h2> </div>
+                    <div><h2 className='text-center text-lg'>{itinerary?.hashtags}</h2> </div>
                     <div className="btn"> 
-                    {itinerary.likes}
+                    {itinerary?.likes}
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 fill-red-500 " fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" /></svg>
                     </div>      
                 </div>
--- src/components/UserItinerary.jsx
+++ src/components/UserItinerary.jsx
@@ -3,17 +3,20 @@ import { useDispatch, useSelector } from 'react-redux'
 import { get_user_by_id } from '../store/actions/userActions'
 
 
-export default function UserItinerary({id}) {
-    const user = useSelector((store) => store.userReducer.user)
+export default function UserItinerary({name, image}) {
+    /* const user = useSelector((store) => store.userReducer.user)
     const dispatch = useDispatch()
     useEffect(()=>{
+        if (!id) {
+          return
+        }
         dispatch(get_user_by_id(id)) 
-    },[])
+    },[]) */
   return (
     <>
     <div className="container">
-        <img src={user.image} className='rounded-full h-24' />
-        <h2 className='text-md '>{user.name}</h2>  
+        <img src={image} className='rounded-full h-24' />
+        <h2 className='text-md '>{name}</h2>  
     </div>
     </>
   )
