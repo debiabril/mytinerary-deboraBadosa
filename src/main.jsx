716c6a7145739f0ce3d7574176da4e863769f0e7
debiabril <debora.badosa@gmail.com>
2023-09-26 16:43:38 -0300

    deploy

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,8 @@ import { store } from './store/store'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store = {store}>
-        <App />
+        {/* <App /> */}
+        <h1>HOLIIIS</h1>
     </Provider>
   </React.StrictMode>,
 )
